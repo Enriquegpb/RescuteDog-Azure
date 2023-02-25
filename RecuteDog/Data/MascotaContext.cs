3a94c1c5ac5b6167a4d9f738080bf1393a97328f
Enrique <enriquegpb5@gmail.com>
2023-02-25 13:25:07 +0100

    añadir contexto bbdd

---
+++ RecuteDog/Data/MascotaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecuteDog.Models;
+
+namespace RecuteDog.Data
+{
+    public class MascotaContext: DbContext
+    {
+        public MascotaContext(DbContextOptions<MascotaContext> options)
+            : base(options) { }
+        DbSet<Mascota> Mascotas { get; set;}
+        DbSet<User> Users { get; set;}
+    }
+}
--- RecuteDog/Models/Mascota.cs
+++ RecuteDog/Models/Mascota.cs
@@ -1,15 +1,27 @@
-﻿namespace RecuteDog.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecuteDog.Models
 {
+    [Table("Mascota")]
     public class Mascota
     {
+        [Column("ID")]
         public int Id { get; set; }
+        [Column("NOMBRE")]
         public string Nombre { get; set; }
+        [Column("RAZA")]
         public string Raza { get; set; }
+        [Column("EDAD")]
         public int Edad { get; set; }
+        [Column("ANCHO")]
         public double Ancho { get; set; }
+        [Column("ALTO")]
         public double Alto { get; set; }
+        [Column("PESO")]
         public double Peso { get; set; }
+        [Column("DESCRIPCION")]
         public string Descripcion { get; set; }
+        [Column("PELIGROSIDAD")]
         public bool Peligrosidad { get; set; }
 
     }

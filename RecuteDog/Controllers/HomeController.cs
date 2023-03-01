557a44977736e9a951ec2a04c6037f5e35dd55ba
Enriquegpb <enriquegpb5@gmail.com>
2023-03-01 09:32:40 +0100

    Codigo Correo Electronico

--- RecuteDog/Controllers/HomeController.cs
+++ RecuteDog/Controllers/HomeController.cs
@@ -14,7 +14,16 @@ namespace RecuteDog.Controllers
 
         public IActionResult Index()
         {
-            List<Mascota> mascotas = this.repo.GetMascotas();
+            //List<Mascota> mascotas = this.repo.GetMascotas();
+            List<Mascota> mascotas= new List<Mascota>();
+            Mascota mascota = new Mascota();
+            Mascota mascota2 = new Mascota();
+            Mascota mascota3 = new Mascota();
+            Mascota mascota4 = new Mascota();
+            mascotas.Add(mascota);
+            mascotas.Add(mascota2);
+            mascotas.Add(mascota3);
+            mascotas.Add(mascota4);
             return View(mascotas);
         }
     }
--- RecuteDog/Controllers/LoginController.cs
+++ RecuteDog/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RecuteDog.Models;
 using RecuteDog.Repositories;
+using System.Net.Mail;
 
 namespace RecuteDog.Controllers
 {
@@ -28,6 +29,23 @@ namespace RecuteDog.Controllers
                 if (usercredentials == null)
                 {
                     await this.repo.NewUser(user);
+                    var mensaje = new MailMessage
+                    {
+                        Subject = "Nuevo Usario",
+                        Body = "Se ha registrado el usuario" + user.Username, 
+                        IsBodyHtml = true,
+                    };
+                    mensaje.From = new MailAddress("enriquegpb5@gmail.com","RecuteDog");
+                    mensaje.To.Add("enriquegpb5@gmail.com");
+
+                    var client = new SmtpClient
+                    {
+                        EnableSsl = true
+                    };
+                    client.Send(mensaje);
+                    mensaje.Dispose();
+                    client.Dispose();
+
                     return RedirectToAction("Index", "Home");
                 }
                 else

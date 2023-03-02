836b485e01cdce1fa2339110110a2283ba143da9
Enriquegpb <enriquegpb5@gmail.com>
2023-03-02 10:15:02 +0100

    Unificar code clase y casa

--- RecuteDog/Controllers/HomeController.cs
+++ RecuteDog/Controllers/HomeController.cs
@@ -14,16 +14,7 @@ namespace RecuteDog.Controllers
 
         public IActionResult Index()
         {
-            //List<Mascota> mascotas = this.repo.GetMascotas();
-            List<Mascota> mascotas= new List<Mascota>();
-            Mascota mascota = new Mascota();
-            Mascota mascota2 = new Mascota();
-            Mascota mascota3 = new Mascota();
-            Mascota mascota4 = new Mascota();
-            mascotas.Add(mascota);
-            mascotas.Add(mascota2);
-            mascotas.Add(mascota3);
-            mascotas.Add(mascota4);
+            List<Mascota> mascotas = this.repo.GetMascotas();
             return View(mascotas);
         }
     }
